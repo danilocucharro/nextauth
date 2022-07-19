733996011a6c743189dca8853129be64f847d5c8
danilo cucharro <danicucharro@outlook.com>
2022-07-19 10:53:50 -0300

    depois que o token de autenticação expira é gerado um novo

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,11 @@
-import axios from "axios";
-import { parseCookies } from 'nookies'
+import axios, { AxiosError } from "axios";
+import { parseCookies, setCookie } from 'nookies'
 
-const cookies = parseCookies();//pegando todos os cookies existentes
+interface AxiosErrorResponse {
+    code?: string;
+}
+
+let cookies = parseCookies();//pegando todos os cookies existentes
 
 export const api = axios.create({
     baseURL: 'http://localhost:3333',
@@ -10,3 +14,35 @@ export const api = axios.create({
     }
 });
 
+api.interceptors.response.use(response =>{
+    return response;
+}, (error: AxiosError<AxiosErrorResponse>) => {
+    if(error.response.status === 401){
+        if(error.response.data?.code === 'token.expired' ){
+            //renover o code
+            cookies = parseCookies();
+
+            const { 'nextauth.refreshToken': refreshToken } = cookies;
+            
+            api.post('/refresh', {
+                refreshToken,
+            }).then(response =>{
+                const { token } = response.data;
+
+                setCookie(undefined, 'nextauth.token', token, {
+                    maxAge: 60 * 60 * 24 * 30, // tempo maximo que o cookie vai existir
+                    path: '/'
+                })
+                setCookie(undefined, 'nextauth.refreshToken', response.data.refreshToken, {
+                    maxAge: 60 * 60 * 24 * 30, // tempo maximo que o cookie vai existir
+                    path: '/'
+                })
+
+                api.defaults.headers['Authorization'] = `Bearer ${token}`;
+            });
+        } else {
+            //deslogar usuario
+
+        }
+    }
+})
